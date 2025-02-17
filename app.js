1e5a3109030df8d72744b0c36dd46fa94639037f
Atchayavijay <atchuvijay83@gmail.com>
2025-02-17 21:43:13 +0530

    project completed

--- app.js
+++ app.js
@@ -23,7 +23,8 @@ config({
 
 app.use(
   cors({
-    origin: "http://localhost:5173/",
+    origin: ["https://auctionplatformproject.netlify.app/",
+      "http://localhost:5173/"],
     methods: ["POST", "GET", "PUT", "DELETE"],
     credentials: true,
   })
